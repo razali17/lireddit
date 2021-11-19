895f5e9a6b3222796bf795da8c16b3bc72e01598
Raza Ali <r.ali@mail.utoronto.ca>
2021-11-18 22:24:17 -0500

    Add register endpoint

---
+++ src/resolvers/user.ts
@@ -0,0 +1,37 @@
+import { MyContext } from "src/types";
+import {
+  Resolver,
+  Query,
+  Ctx,
+  Arg,
+  Mutation,
+  InputType,
+  Field,
+} from "type-graphql";
+import { User } from "../entities/User";
+import argon2 from "argon2";
+
+@InputType()
+class UsernamePasswordInput {
+  @Field()
+  username: string;
+  @Field()
+  password: string;
+}
+
+@Resolver()
+export class UserResolver {
+  @Mutation(() => User)
+  async register(
+    @Arg("options") options: UsernamePasswordInput,
+    @Ctx() { em }: MyContext
+  ) {
+    const hashedPassword = await argon2.hash(options.password);
+    const user = em.create(User, {
+      username: options.username,
+      password: hashedPassword,
+    });
+    await em.persistAndFlush(user);
+    return user;
+  }
+}
